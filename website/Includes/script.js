44fd731d41257e650a6b4a8012465d761e35063d
Galvantua <34282639+Galvantua@users.noreply.github.com>
2018-11-14 16:04:33 -0500

    Add files via upload

---
+++ website/Includes/script.js
@@ -0,0 +1,9 @@
+function index() {
+	window.location.href="./index.html";
+};
+function projects() {
+	window.location.href="./projects.html";
+};
+function signin() {
+	window.location.href="./sign-in.html";
+};
