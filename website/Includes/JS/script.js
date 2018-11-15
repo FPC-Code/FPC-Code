39179f98be56063c5d47deed7e212d4d53f7add5
Galvantua <34282639+Galvantua@users.noreply.github.com>
2018-11-15 08:45:17 -0500

    Update script.js

--- website/Includes/JS/script.js
+++ website/Includes/JS/script.js
@@ -7,3 +7,26 @@ function projects() {
 function signin() {
 	window.location.href="./sign-in.html";
 };
+function index() {
+	window.location.href="./index.html";
+};
+function projects() {
+	window.location.href="./projects.html";
+};
+function signin() {
+	window.location.href="./sign-in.html";
+};
+	var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbxzzGc137WpdpYfJwyRHlybQvP9taBfM7tGdIB4qCLvNu1T128L/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
