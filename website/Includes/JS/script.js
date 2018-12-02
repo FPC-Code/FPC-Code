3fcbb3f43c7bb93e927bb867f7af19f43b854ab0
Galvantua <galvantua@gmail.com>
2018-12-02 17:20:57 -0500

    update js

--- website/Includes/JS/script.js
+++ website/Includes/JS/script.js
@@ -7,15 +7,15 @@ function projects() {
 function signin() {
 	window.location.href="./sign-in.html";
 };
-var $form = $('form#signup'),
-url = 'https://script.google.com/macros/s/AKfycbxzzGc137WpdpYfJwyRHlybQvP9taBfM7tGdIB4qCLvNu1T128L/exec'
+//var $form = $('form#signup'),
+//url = 'https://script.google.com/macros/s/AKfycbxzzGc137WpdpYfJwyRHlybQvP9taBfM7tGdIB4qCLvNu1T128L/exec'
 
 //$('#submit').on('click', function(e) {
 //  e.preventDefault();
 //  var jqxhr = $.ajax({
 //    url: url,
 //    method: "GET",
-//   dataType: "json",
+//    dataType: "json",
 //    data: $form.serializeObject()
 //  }).success(
 //    alert("Your data has been submitted")
@@ -23,5 +23,5 @@ url = 'https://script.google.com/macros/s/AKfycbxzzGc137WpdpYfJwyRHlybQvP9taBfM7
 //})
 function submit () {
 	var name = document.getElementsByName(name).text;
-	//var newdoc = ;
+	
 };
