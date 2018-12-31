935b282b6f9aced9f12fb1b86bcb5d407a69ee4b
Galvantua <galvantua@gmail.com>
2018-12-30 20:51:04 -0500

    add php

--- website/Includes/JS/script.js
+++ website/Includes/JS/script.js
@@ -7,9 +7,3 @@ function projects() {
 function signin() {
 	window.location.href="./sign-in.html";
 };
-function submit () {
-	var name = document.getElementsByName(name).text;
-	var email = document.getElementsByName(email).text;
-	var type = document.getElementsByName(sign-up-type)
-	var access = document.getElementById()
-};
