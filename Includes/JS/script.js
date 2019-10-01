bd61fdc8771e7526587f8389c42b4d0d253daa0d
Robert Miller <robbie@millertribe.com>
2019-10-01 19:24:51 -0400

    Imported New File

---
+++ Includes/JS/script.js
@@ -0,0 +1,17 @@
+var otherbutton = document.getElementById(otherbutton);
+var other = document.getElementsByClassName(other);
+
+function redirect(url) {
+	window.location.href = url;
+}
+
+function emailofficers(email) {
+	window.location.href = "mailto:" + email;
+}
+
+function setClass(className, newClass) {
+	var items = document.getElementsByClassName(className);
+	for (var i = 0; i < items.length; i++) {
+		items[i].classList.toggle(newClass);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CCWebstite
+
+## Contributers
+
+Alan Hale
+Tyler Lopez
+Morgan Hale
+Draven Monterrosa
+Gavin Morton
+Robbie Miller
+Hughes Le
+Will St. Peter
