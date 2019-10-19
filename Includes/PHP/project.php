03bb96a3da6b2b64b5bd3cfbfb34be33fb96ae7f
Galvantua <galvantua@gmail.com>
2019-10-19 14:38:24 -0400

    fix redirects

--- Includes/PHP/project.php
+++ Includes/PHP/project.php
@@ -13,7 +13,7 @@ if (isset($_POST['submit'])) {
 	$body = $name." has requested a website:";
 
 	mail($mailto, "Website Request", "\n".$body."\n\n".$signuptype."\n".$webtype."\n\n".$webtypeother."\n\n".$access, $header);
-	header("Location: ../../");
+	header("Location: ../../thanks.html");
 } else {
 	header("Location: ../../sorry.html");
 }
