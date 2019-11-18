d72d9749b65fb1a0d3a57bebda4ac9cf426bc652
Robert Miller <robbie@millertribe.com>
2019-11-17 20:10:04 -0500

    Fixed PHP Locations

--- Includes/PHP/project.php
+++ Includes/PHP/project.php
@@ -13,8 +13,8 @@ if (isset($_POST['submit'])) {
 	$body = $name." has requested a website:";
 
 	mail($mailto, "Website Request", "\n".$body."\n\n".$email."\n\n".$signuptype."\n".$webtype."\n\n".$webtypeother."\n\n".$access, $header);
-	header("Location: ../../thanks-projects/");
+	header("Location: /thanks-projects/");
 } else {
-	header("Location: ../../sorry/");
+	header("Location: /sorry/");
 }
 ?>
